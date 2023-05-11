a3260850c2ec2412a65ff96c94e67d708ecfe906
moassamo <moassermo@gmail.com>
2023-05-11 16:02:50 -0600

    done

--- server/server.js
+++ server/server.js
@@ -1,37 +1,52 @@
 const express = require("express");
 const path = require("path");
-const db = require("./config/connection");
-
-// add apollo server
+//import apollo server
 const { ApolloServer } = require("apollo-server-express");
-const { typeDefs, resolvers } = require("./schema");
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 
+//db connection
+const db = require("./config/connection");
+
+// const routes = require('./routes');
+
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// add apollo middleware
-const startServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
-  await server.start();
-  server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-startServer();
+//apply apollo server with express app
+server.applyMiddleware({ app });
 
+//middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
+app.use(express.static(buildPath));
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+// app.use(routes);
+
+//get all
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
