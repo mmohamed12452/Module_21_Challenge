eea2ece01ef600c756d1bf06636efc5491cffc4f
moassamo <moassermo@gmail.com>
2023-05-11 14:20:15 -0600

    unknown error

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const db = require("./config/connection");
 
 // add apollo server
 const { ApolloServer } = require("apollo-server-express");
-const { typeDefs, resolvers } = require("./schemas");
+const { typeDefs, resolvers } = require("./schema");
 const { authMiddleware } = require("./utils/auth");
 
 const app = express();
