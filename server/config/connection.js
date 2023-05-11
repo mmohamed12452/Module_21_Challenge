8cf33d32d3fe45db13ce81393dec65f0bb2b7da1
moassamo <moassermo@gmail.com>
2023-05-11 13:44:50 -0600

    issue with schema typeDefs

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,6 +5,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googleboo
     useUnifiedTopology:true,
     useCreateIndex:true,
     useFindAndModify:false
-});
+ });
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
 
 // add apollo server
 const { ApolloServer } = require("apollo-server-express");
-const { typeDefs, resolvers } = require("./schemas");
+const { typeDefs, resolvers } = require("./schema");
 const { authMiddleware } = require("./utils/auth");
 
 const app = express();
@@ -28,10 +29,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-db.once("open", () => {
+app.use(routes);
+
+db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
